46b986923f03413d23077421d2aad0dd15cb59c3
Nick Tardif <nicktardif@gmail.com>
2015-10-24 11:38:03 -0400

    Add syntax support for Dockerfiles

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ ln -s `pwd`/.vimrc ~/.vimrc
 ln -s `pwd`/.Xdefaults ~/.Xdefaults
 
 cp -r `pwd`/.vim ~/
+git clone https://github.com/ekalinin/Dockerfile.vim.git ~/.vim/bundle/Dockerfile
 
 git config --global user.name "Nick Tardif"
 git config --global user.email "nicktardif@gmail.com"
